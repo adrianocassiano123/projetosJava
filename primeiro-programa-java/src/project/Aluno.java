68a18783bea21e19d5176eb831a747e4420f3466
adriano cassiano <adrianocassiano88@gmail.com>
2021-07-28 18:47:12 -0300

    Primeiro commit

---
+++ primeiro-programa-java/src/project/Aluno.java
@@ -0,0 +1,100 @@
+package project;
+
+public class Aluno {
+
+	String nome;
+	int idade;
+	String nomeDaMae;
+	String nomeDoPai;
+	String dataMatricula;
+	String nomeDaEscola;
+	double nota1;
+	double nota2;
+	double nota3;
+	double nota4;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getNomeDaMae() {
+		return nomeDaMae;
+	}
+	public void setNomeDaMae(String nomeDaMae) {
+		this.nomeDaMae = nomeDaMae;
+	}
+	public String getNomeDoPai() {
+		return nomeDoPai;
+	}
+	public void setNomeDoPai(String nomeDoPai) {
+		this.nomeDoPai = nomeDoPai;
+	}
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	public String getNomeDaEscola() {
+		return nomeDaEscola;
+	}
+	public void setNomeDaEscola(String nomeDaEscola) {
+		this.nomeDaEscola = nomeDaEscola;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	
+//	Método retorna a média do aluno
+	public double getMediaNota() {
+		
+		return (nota1+nota2+nota3+nota4)/4;
+		
+	}
+	
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media>=70) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ primeiro-programa-java/src/project/Projeto.java
@@ -0,0 +1,45 @@
+package project;
+
+import javax.swing.JOptionPane;
+
+public class Projeto {
+
+	public static void main(String[] args) {
+
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String idade = JOptionPane.showInputDialog("Qual a idade do aluno");
+		String nomeEscola = JOptionPane.showInputDialog("Qual o nome da escola?");
+		String matricula = JOptionPane.showInputDialog("Qual o numero da matricula?");
+		String nomeMae = JOptionPane.showInputDialog("Qual o nome da mae do aluno?");
+		String nomePai = JOptionPane.showInputDialog("Qual o nome do pai do aluno?");
+		
+		String nota1 = JOptionPane.showInputDialog("Nota 1");
+		String nota2 = JOptionPane.showInputDialog("Nota 2");
+		String nota3 = JOptionPane.showInputDialog("Nota 3");
+		String nota4 = JOptionPane.showInputDialog("Nota 4");
+		
+		
+		
+		
+		
+		
+		Aluno aluno1 = new Aluno();
+		
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade)); // passar string para inteiro
+		aluno1.setDataMatricula(matricula);
+		aluno1.setNomeDaEscola(nomeEscola);
+		aluno1.setNomeDaMae(nomeMae);
+		aluno1.setNomeDoPai(nomePai);
+		
+		aluno1.setNota1(Double.parseDouble(nota1)); //passar de string para double
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
+		
+		System.out.println(aluno1.getNome());
+
+	}
+
+}
